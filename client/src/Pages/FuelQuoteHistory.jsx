158da92f0871ebe98a819f7f5d82963effc8bd10
KingKennyboy <95037426+KingKennyboy@users.noreply.github.com>
2023-09-20 19:39:07 -0500

    Fuel Quote History commit

--- client/src/Pages/FuelQuoteHistory.jsx
+++ client/src/Pages/FuelQuoteHistory.jsx
@@ -5,7 +5,47 @@ function FuelQuoteHistory() {
     return (
         <div class="container">
             <h2>Fuel Quote History</h2>
+             <Link to="/home"> Back to Home Page</Link>
+             <div class="p-3 border bg-light">
+                <div class="divTable">
+                        <div class="divTableBody">
+                        <div class="divTableRow">
+                        <div class="divTableCell">Gallons Requested</div>
+                        <div class="divTableCell">Delivery Address</div>
+                        <div class="divTableCell">Delivery Date</div>
+                        <div class="divTableCell">Suggested Price</div>
+                        <div class="divTableCell">Total Amount Due</div>
+                        </div>
+                        
+                        <div class="divTableRow">
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        </div>
+
+                        <div class="divTableRow">
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        </div>
+
+                        <div class="divTableRow">
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        <div class="divTableCell">&nbsp;</div>
+                        </div>
+                    </div>
+                </div>              
+                
+             </div>
         </div>
+        
     )
 }
 
