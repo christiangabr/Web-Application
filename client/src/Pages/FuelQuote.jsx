9b0ee2ebc96f79293efafde31aba230fd5872d34
Erick Pham <Erick.d.pham@gmail.com>
2023-09-22 21:34:12 -0500

    clean up frontend

--- client/src/Pages/FuelQuote.jsx
+++ client/src/Pages/FuelQuote.jsx
@@ -1,3 +1,32 @@
+import React from 'react'
+import Calendar from '../components/Calendar/Calendar'
+
+function FuelQuote() {
+    return (
+        <div class="container">
+            <h2>Fuel Quote</h2>
+            <div class="row">
+                <div class="col">
+                    <h3>Gallons Requested</h3>
+                    <input type="text" required></input>
+                    <h3>Delivery Address</h3>
+                    <p>[PlaceHolder]</p>
+                    <h3>Delivery Date</h3>
+                    <Calendar />
+                </div>
+                <div class="col">
+                    <h3>Suggested Price</h3>
+                    <input type="text" required></input>
+                    <h3>Total Amount Due</h3>
+                    <input type="text" required></input>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FuelQuote;
+
 // import React from 'react'
 // import { Link } from 'react-router-dom'
 
@@ -5,46 +34,16 @@
 //     return (
 //         <div class="container">
 //             <h2>Fuel Quote</h2>
-//             <Link to="/home"> Back to Home Page</Link> <br/><br/>
-//             <div class="row">
-//                 <div class="col">
-//                     <h3>Gallons Requested</h3>
-//                     <input type="text" required></input>
-//                     <h3>Delivery Address</h3>
-//                     <input type="text" required></input>
-//                     <h3>Delivery Date</h3>
-//                     <input type="text" required></input>
-//                 </div>
-//                 <div class="col">
-//                     <h3>Suggested Price</h3>
-//                     <input type="text" required></input>
-//                     <h3>Total Amount Due</h3>
-//                     <input type="text" required></input>
-//                 </div>
+//             <div class="p-3 border bg-light">
+//                 <h3>Gallons Requested</h3>
+//                 <input type="text" required></input>
+//                 <h3>Delivery Address</h3>
+//                 <h3>Delivery Date</h3>
+//                 <h3>Suggested Price</h3>
+//                 <h3>Total Amount Due</h3>
 //             </div>
 //         </div>
 //     )
 // }
 
 // export default FuelQuote;
-
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-function FuelQuote() {
-    return (
-        <div class="container">
-            <h2>Fuel Quote</h2>
-            <div class="p-3 border bg-light">
-                <h3>Gallons Requested</h3>
-                <input type="text" required></input>
-                <h3>Delivery Address</h3>
-                <h3>Delivery Date</h3>
-                <h3>Suggested Price</h3>
-                <h3>Total Amount Due</h3>
-            </div>
-        </div>
-    )
-}
-
-export default FuelQuote;
