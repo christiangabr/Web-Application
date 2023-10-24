de1292b62aacb459de43e22875ed890735b22016
Christian Gabriana <142938707+christiangabr@users.noreply.github.com>
2023-10-24 14:30:37 -0500

    login/register fix

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -1,22 +1,10 @@
 const mongoose = require("mongoose");
 
 const EmployeeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-}, {timestamps: true});
+  name: String,
+  email: String,
+  password: String
+})
 
-
-module.exports  = mongoose.model("employee", EmployeeSchema);
-
+const EmployeeModel = mongoose.model("employees", EmployeeSchema);
+module.exports = EmployeeModel;
