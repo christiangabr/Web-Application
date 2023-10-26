132958111412632554f254c291f06963b2961e99
Erick Pham <Erick.d.pham@gmail.com>
2023-10-25 21:31:20 -0500

    test 2 update patch

--- server/Controllers/profile.js
+++ server/Controllers/profile.js
@@ -1,18 +1,8 @@
 const profile = require('../models/profileModel');
 
-
 const checkProfile = async (req,res,next) => {
   try {  
     const newProfile = new profile({
-<<<<<<< HEAD
-      email: "helloworld@gmail.com",
-      fullName: "King Kenny",
-      address1: "123 Elm Street",
-      address2: "123 Elm Street",
-      city: "Houston",
-      state: "TX",
-      zipCode: "77777" 
-=======
       email: "john@io.com",
       fullName: "John Dee",
       address1: "fall123",
@@ -21,7 +11,6 @@ const checkProfile = async (req,res,next) => {
       state: "TX",
       zipCode: "77204"
 
->>>>>>> fff459d (db patch update)
     })
     res.json(newProfile)
   } catch(err) {
@@ -29,10 +18,4 @@ const checkProfile = async (req,res,next) => {
   }
 }
 
-<<<<<<< HEAD
-
-
-module.exports = {checkProfile};
-=======
 module.exports = { checkProfile };
->>>>>>> fff459d (db patch update)
--- server/Routes/profileRoute.js
+++ server/Routes/profileRoute.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const { checkProfile } = require('../Controllers/profile');
 
@@ -8,17 +7,4 @@ const router = express.Router();
 router.get('/', checkProfile)
 
 
-=======
-const express = require('express');
-const { checkProfile } = require('../Controllers/profile')
-
-const router = express.Router();
-
-
-router.get('/', checkProfile)
-
-
-
-
->>>>>>> fff459d (db patch update)
 module.exports = router;
